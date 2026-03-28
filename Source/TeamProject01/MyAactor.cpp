182e35f29e28fd453ef2d8778be61af5b80e29c3
MIN <nakbkqwe21@gmail.com>
2026-03-29 00:08:28 +0900

    [Mine] MyAactor 클래스 수정

--- Source/TeamProject01/MyAactor.cpp
+++ Source/TeamProject01/MyAactor.cpp
@@ -1,4 +1,4 @@
-// MyAactor.cpp
+// MyActor.cpp
 
 
 #include "MyActor.h"
@@ -13,11 +13,13 @@ void AMyActor::BeginPlay()
 {
 	Super::BeginPlay();
 
+	UE_LOG(LogTemp, Warning, TEXT("AMyActor has begun play!"));
 }
 
 void AMyActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	UE_LOG(LogTemp, Warning, TEXT("AMyActor is ticking!"));
 }
 
